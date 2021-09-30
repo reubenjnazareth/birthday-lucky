b713b0320bde04f799ec6ef12d26007d05cf2cff
rnaz25 <rnaz25@gmail.com>
2021-09-30 20:07:09 +0530

    initial comit for js

---
+++ index.js
@@ -0,0 +1,4 @@
+const dateOfBirth = document.querySelector(date-of-birth);
+const luckyNumber = dociment.querySelector("#luck-number");
+const checkNumberButton = document.querySelector("#check-number");
+
