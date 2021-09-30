e9157f48c5cf7a92b40ecca783ab9f73e0b2faf0
rnaz25 <rnaz25@gmail.com>
2021-09-30 21:06:38 +0530

    minor

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-const dateOfBirth = document.querySelector(date-of-birth);
-const luckyNumber = dociment.querySelector("#luck-number");
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
+function compareValues(sum, luckyNumber) {
+    if(sum % luckyNumber === 0) {
+        outputBox.innerText = "Your birthday is Lucky! :) ";
+    }
+    }
+    else {
+        outputBox.innerText = "Your birthday is not lucky :/ ";
+    }
+}
+
+
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum&&dob) {
+    compareValues(sum, Number(luckyNumber.value))
+    } else {
+        outputBox.innerText="Please enter both fields!";
+    }
+}
+
+function calculateSum(dob) {
+    dob = date.replaceAll("-", "");
+    let sum = 0;
+    for(let index=0; index<=dob.length; index++ ) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+    
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky)
